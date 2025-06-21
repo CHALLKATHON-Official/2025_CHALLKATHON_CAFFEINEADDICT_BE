515fec2a69adb4a00b3fc0be4930905f0b836593
dong99u <qkrehdrb0813@gmail.com>
2025-06-21 10:21:04 +0900

    :art: 시작  - Spring Initializr 세팅

---
+++ src/main/kotlin/com/challkathon/caffeine/CaffeineApplication.kt
@@ -0,0 +1,11 @@
+package com.challkathon.caffeine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CaffeineApplication
+
+fun main(args: Array<String>) {
+	runApplication<CaffeineApplication>(*args)
+}
---
+++ src/test/kotlin/com/challkathon/caffeine/CaffeineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.challkathon.caffeine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CaffeineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
