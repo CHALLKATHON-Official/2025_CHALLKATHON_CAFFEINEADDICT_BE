81bce5e8b809b86f343199f5bd20db458755617f
이승주 <eun07213@gmail.com>
2025-06-23 22:34:00 +0900

    feat(family): family 양방향 매핑 추가

--- src/main/kotlin/com/challkathon/momento/domain/family/entity/Family.kt
+++ src/main/kotlin/com/challkathon/momento/domain/family/entity/Family.kt
@@ -1,5 +1,7 @@
 package com.challkathon.momento.domain.family.entity
 
+import com.challkathon.momento.domain.question.entity.mapping.FamilyQuestion
+import com.challkathon.momento.domain.todo.mapping.FamilyTodoList
 import jakarta.persistence.*
 
 @Entity
@@ -16,4 +18,12 @@ class Family(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "family_id", nullable = false)
     val id: Long = 0
+
+    // Family.kt
+    @OneToMany(mappedBy = "family", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val questions: MutableList<FamilyQuestion> = mutableListOf()
+
+    @OneToMany(mappedBy = "family", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val todos: MutableList<FamilyTodoList> = mutableListOf()
+
 }
