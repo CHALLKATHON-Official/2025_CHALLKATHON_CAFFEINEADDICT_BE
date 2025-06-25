22fe8cdaf5e492b4e67bc2c92b4715e3e8a3e2b5
이승주 <eun07213@gmail.com>
2025-06-25 12:19:54 +0900

    feature(family): Family예외 구현

--- src/main/kotlin/com/challkathon/momento/domain/family/exception/FamilyException.kt
+++ src/main/kotlin/com/challkathon/momento/domain/family/exception/FamilyException.kt
@@ -3,6 +3,20 @@ package com.challkathon.momento.domain.family.exception
 import com.challkathon.momento.domain.family.exception.code.FamilyErrorStatus
 import com.challkathon.momento.global.exception.BaseException
 
-class FamilyException(
-    code: FamilyErrorStatus
+// 공통 상위 예외
+open class FamilyException(
+    code: FamilyErrorStatus,
+    override val message: String = code.getCode().message
 ) : BaseException(code)
+
+class FamilyAlreadyJoinedException(
+    override val message: String = "이미 가족에 소속되어 있습니다."
+) : FamilyException(FamilyErrorStatus.FAMILY_ALREADY_JOINED, message)
+
+class FamilyInviteCodeNotFoundException(
+    override val message: String = "존재하지 않는 초대 코드입니다."
+) : FamilyException(FamilyErrorStatus.INVITE_CODE_NOT_FOUND, message)
+
+class FamilyNotJoinedException(
+    override val message: String = "가족에 소속되어 있지 않습니다."
+) : FamilyException(FamilyErrorStatus.FAMILY_NOT_JOINED, message)
